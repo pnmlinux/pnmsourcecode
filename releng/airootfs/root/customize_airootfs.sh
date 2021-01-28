cd02cc17177cff557a35d7b95d59b2c5e40e7684
suleymanfatih <sp.samu152@hotmail.com>
2021-01-28 03:49:43 +0300

    fix : customize

--- releng/airootfs/root/customize_airootfs.sh
+++ releng/airootfs/root/customize_airootfs.sh
@@ -218,6 +218,16 @@ function deleteprograms () {
 
 }
 
+function linuxpresetfix () {
+
+[ $UID != 0 ] && { echo "run it as root privs" ; exit 1 ;}
+
+for i in $( seq 1 "$(ls /boot/ | grep "$(uname -r)" | wc -l)" ) ; do
+    cp "/boot/$(ls /boot/ | grep "$(uname -r)" |  awk "NR==${i}")" "/boot/$(ls /boot/ | grep "$(uname -r)" | sed "s/$(uname -r)/linux/g" | awk "NR==${i}")"
+done
+
+}
+
 
 deleteXfceWallpapers
 layout deleteXfceWallpapers
@@ -258,5 +268,5 @@ layout getNewMirrorCleanAndUpgrade
 renameOSFunc
 deleteprograms
 layout deleteprograms
-
-
+linuxpresetfix
+layout linuxpresetfix
